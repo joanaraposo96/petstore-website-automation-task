e65e4fd95f10a38f9d4fc9b98efef5d298b6758d
joanaraposo96 <joanaraposo96@outlook.pt>
2026-04-01 17:32:49 +0200

    refactored code for Search Product class and test files.

--- classes/SearchProduct.js
+++ classes/SearchProduct.js
@@ -1,35 +1,32 @@
 import { expect } from "@playwright/test";
+import Homepage from "./Homepage";
 
-class SearchProduct {
-    page;
-    searchField;
-    searchButton;
-    nameTableData;
-    searchedValue;
-    descriptionTableData;
-    randomSelectedItem;
-
+class SearchProduct extends Homepage {
     constructor(page) {
-        this.page = page;
-        this.searchField = page.getByRole('textbox');
-        this.searchButton = page.getByRole('button', { name: 'Search' });
-        this.nameTableData = page.locator('table tbody tr td:nth-child(3)');
-        this.searchedValue = null;
-        this.descriptionTableData = page.locator('table tbody tr td:nth-child(1)')
-        this.randomSelectedItem = null;
+        this.searchField = page.locator('input[name="keyword"]');
+        this.searchButton = page.locator('input[name="searchProducts"]');
+        this.linkToProductDescription = (productDescription) => page.locator(`tbody a:has-text(${productDescription})`); 
+        // this.nameTableData is a function, NOT a locator yet!
+        this.nameTableData = (productName) => page.locator(`tbody td:has-text(${productName})`); // Check if the element’s text CONTAINS the given string.
     }
 
     async searchPet(value){
-        this.searchedValue = value;
         await this.searchField.fill(value);
         await this.searchButton.click();
+        const nameTableDataLocator = this.nameTableData(value); // The searched value now equals the value passed onto this.nameTableData(productName) function.
+    }
+
+    async expectSearchToBeSuccessful() {
+        await expect(nameTableDataLocator).toBeVisible(); // If there are any corresponding search results, that data will be shown under the "Name" table.
     }
 
     async selectRandomProduct() {
-        const locatorCount = await this.descriptionTableData.count();
-        const randomIndex = Math.floor(Math.random() * locatorCount);
-        this.randomSelectedItem = this.descriptionTableData.nth(randomIndex);
-        await this.randomSelectedItem.click();
+        const convertToList = await this.linkToProductDescription.allTextContent(); // Convert all text values to a string.
+        const randomValue = convertToList[(Math.floor(Math.random() * convertToList.length))]; // Retrieve a random text string value
+        const linkToProductDescriptionLocator = this.linkToProductDescription(randomValue); 
+        // Random text value now equals (productDescription) variable. 
+        // Assign const to this.nameTableData(productDescription) function so we can now use it as a locator and interact with it.
+        await linkToProductDescriptionLocator.click();
     }
 }
 
--- tests/searchProduct.spec.js
+++ tests/searchProduct.spec.js
@@ -1,17 +1,30 @@
 import { test, expect } from '@playwright/test';
 import SearchProduct from '../classes/SearchProduct';
+import Homepage from '../classes/Homepage';
 
-test('Pet Store', async ({ page }) => {
-  const product = new SearchProduct(page);
+test.describe('Search Products', () => {
+  let homepage;
+  let product;
 
-  await page.goto("https://petstore.octoperf.com/actions/Catalog.action");
+  test.beforeEach(async ({ page }) => { // Single browser tab
+    homepage = new Homepage(page);
+    product = new SearchProduct(page);
 
-  await expect(page).toHaveTitle("JPetStore Demo");
+    // Passing the active browser tab into SearchProduct class so it can:
+    // - locate elements;
+    // - click buttons;
+    // - type into inputs;
+    // - assert UI behavior.
+    // The is what we call the Page Object Model (POM). 
+    // It is a design pattern in test automation where each page (or component) is represented by a class.
+    // Playwright’s page object is used inside that class to define reusable actions (methods) and locators.
 
-  await product.searchPet('e');
+  });
 
-  const texts = await product.nameTableData.allTextContents(); //returns a list
-  texts.forEach(text => expect(text).toContain(product.searchedValue));
-
-  await product.randomSelectedItem;
+  test('Successfuly search for a random product', async ({ page }) => {
+    await homepage.navigateToHomepage();
+    await product.searchPet('fish');
+    await product.expectSearchToBeSuccessful();
+    await product.randomSelectedItem();
+  });
 });
